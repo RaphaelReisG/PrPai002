b7164956cf088fc64280596c00dc3513bc407ad5
RaphaelReisG <86501038+RaphaelReisG@users.noreply.github.com>
2023-03-23 08:07:20 -0300

    limpeza

--- resources/js/vueScript.js
+++ resources/js/vueScript.js
@@ -4,137 +4,48 @@ var app = new Vue({
     el: '#app',
     data: {
         idUsuario: "{{ Auth::user()->id }}",
+        titulo: "",
+        acaoObjeto: "",
+        nomeObjeto: "",
 
-            banana: false,
-            titulo: "",
-            acaoObjeto: "",
-            nomeObjeto: "",
+        resposta: [{}],
 
-            resposta: [{}],
+        error: null,
+        carregando: false,
+        carregandoGeral: false,
+        objetos: [{}],
+        modalErro: false,
+        modalSucesso: false,
+        modelObjetos:[{
+            id: "",
 
-            error: null,
-            carregando: false,
-            carregandoGeral: false,
-            objetos: [{}],
-            modalErro: false,
-            modalSucesso: false,
-            modelObjetos:[{
-                id: "",
+            name: "", email: "", senha: "", confirmaSenha: "",
 
-                name: "", email: "", senha: "", confirmaSenha: "",
+            company_name: "", cnpj: "",
 
-                company_name: "", cnpj: "",
+            symbol: "", startTime: "", endTime: "", limite: ""
 
-                setup: "", simbolo: "", emocao: "", compraVenda: "", quantidade: "", valorEntrada: "", valorSaida: "", taxa: "",  setup: "", dtEntrada: "",
-                dtSaida: "", fdEmocional: "", fdImediato: "",
+        }],
+        index: "",
 
-                key: "", secret: "",
-
-                symbol: "", startTime: "", endTime: "", limite: "",
-
-                nomeSetup: "",
-                nomeEmocao: "",
-
-            }],
-            index: "",
-
-            buscaApi: false,
-            apiObjetos: [{}],
-            credencialApi: [{}],
-            setups: [{}],
-            emocaos: [{}],
-            compra_venda: [{valor: "Compra"}, {valor: "Venda"}],
-
-            mostrarSenha: "password",
-
-            teste:  [{}],
-            nomeSetup: ""
+        mostrarSenha: "password"
     },
     methods: {
         defineClasse: function(classe, titulo ){
-            //alert('01');
-            //this.objetos = null;
-            //this.limparGeral();
             this.nomeObjeto = classe;
             this.titulo = titulo;
-
             if(classe != ''){
-                    this.carregarObjeto(classe);
-
-
+                this.carregarObjeto(classe);
             }
-
         },
         carregarObjeto: function(classe){
-            //alert('02');
             this.objetos = null;
             this.carregandoGeral = true;
             var url;
-            if(classe == "diario"){
-                //alert("passou aki");
-                url = '/api/diario_usuario/'+this.idUsuario;
-            }
-            else{
-                //url = '/api/'+classe+'/'+this.idUsuario;
-                url = '/api/'+classe;
-            }
-
+            url = '/api/'+classe;
+            
             fetch(url).then((res) => res.json())
                     .then((data) => this.objetos = data).finally(this.carregandoGeral = false);
-
-            //this.carregandoGeral = false;
-
-        },
-        carregarApiBinance: function(){
-            this.carregando = true;
-            this.buscaApi = false;
-            if(this.modelObjetos[0]['limite'] == null){
-                this.modelObjetos[0]['limite'] = 0;
-            }
-            if(this.modelObjetos[0]['startTime'] == null || this.modelObjetos[0]['startTime'] == ''){
-                var startTime = 0;
-            }
-            else{
-                var startTime = new Date(this.modelObjetos[0]['startTime']).getTime();
-            }
-
-            if(this.modelObjetos[0]['endTime'] == null || this.modelObjetos[0]['endTime'] == ''){
-                var endTime = 0;
-            }
-            else{
-                var endTime = new Date(this.modelObjetos[0]['endTime']).getTime();
-            }
-
-            var url = '/api/importar/'+this.idUsuario+'/'+this.modelObjetos[0]['symbol']+'/'+this.modelObjetos[0]['limite']+'/'+startTime+'/'+endTime;
-            fetch(url).then((res) => res.json())
-                .then((data) => this.apiObjetos = data);
-
-            this.carregando = false;
-            this.buscaApi = true;
-
-            alert(url);
-        },
-        carregaCredencialApi: function(){
-            var url = '/api/credencialapibinance/'+this.idUsuario;
-            //alert(url);
-            fetch(url).then((res) => res.json())
-                    .then((data) => this.credencialApi = data);
-        },
-        carregaSetup: function(){
-            this.carregandoGeral = true;
-            var url = '/api/setup/'+this.idUsuario;
-            //alert(url);
-            fetch(url).then((res) => res.json())
-                    .then((data) => this.setups = data);
-            this.carregandoGeral = false;
-        },
-        carregaEmocao: function(){
-            this.carregandoGeral = true;
-            var url = '/api/emocao/'+this.idUsuario;
-            //alert(url);
-            fetch(url).then((res) => res.json())
-                    .then((data) => this.emocaos = data);
-            this.carregandoGeral = false;
         },
         escolheAcaoObjeto: function(acao, classe){
             if(this.verificaDados(classe) == true){
@@ -177,11 +88,6 @@ var app = new Vue({
 
             alert(url);
 
-            //fetch(url, { method: 'POST'} ).then((res) => this.error = res);
-
-            //fetch(url).then((res) => res.json())
-            //.then((data) => this.objetos = data);
-
             if(this.error == null){
                 this.limparModal();
                 this.carregando = false;
@@ -203,56 +109,17 @@ var app = new Vue({
             }
 
             //alert("01 - "+index);
-            if(this.acaoObjeto == 'AddAPI'){
-                if(this.apiObjetos[index]['side'] == 'BUY'){
-                    this.modelObjetos[0]['compraVenda'] = "Compra";
-                }
-                else if(this.apiObjetos[index]['side'] == 'SELL'){
-                    this.modelObjetos[0]['compraVenda'] = "Venda";
-                }
-
-                var data = new Date(this.apiObjetos[index]['time']);
-                data = data.toLocaleString();
-
-                //new Date(obj[valor.conteudo]).toLocaleString()
-
-                var dataFormatada = new Date(data);
-
-                //alert(dataFormatada);
-                //alert(data);
-
-                //this.modelObjetos['dataRegistro'] = this.apiObjetos[index]['time'];
-                //  this.modelObjetos[0]['dtEntrada'] = data;
-                //this.modelObjetos['dataRegistro'] = this.objetos[0]['dataRegistro'];
-
-                //alert(this.modelObjetos['dataRegistro']);
-
-                //this.modelObjetos['emocao'] = this.objetos[index]['emocao'];
-                //this.modelObjetos['compraVenda'] = this.objetos[index]['compraVenda'];
-                //this.modelObjetos['quantidade'] = this.apiObjetos[index]['qty'];
-                //this.modelObjetos['pontos'] = this.objetos[index]['pontos'];
-                this.modelObjetos[0]['valorEntrada'] = this.apiObjetos[index]['price'];
-                //this.modelObjetos['taxa'] = this.objetos[index]['taxa'];
-                //this.modelObjetos['dtEntrada'] = this.objetos[index]['dtEntrada'];
-                //this.modelObjetos['dtSaida'] = this.objetos[index]['dtSaida'];
-                //this.modelObjetos['fdEmocional'] = this.objetos[index]['fdEmocional'];
-                //this.modelObjetos['fdImediato'] = this.objetos[index]['fdImediato'];
-            }
-
-            else if(this.nomeObjeto == "cliente"){
+            if(this.nomeObjeto == "cliente"){
                 this.modelObjetos[0]['name'] = this.objetos['data'][index]['name'];
                 this.modelObjetos[0]['cnpj'] = this.objetos['data'][index]['cnpj'];
                 this.modelObjetos[0]['email'] = this.objetos['data'][index]['user']['email'];
                 this.modelObjetos[0]['company_name'] = this.objetos['data'][index]['company_name'];
             }
-
             else if(this.nomeObjeto == "administrador"){
                 this.modelObjetos[0]['name'] = this.objetos['data'][index]['name'];
                 this.modelObjetos[0]['email'] = this.objetos['data'][index]['user']['email'];
                 this.modelObjetos[0]['company_name'] = this.objetos['data'][index]['company_name'];
             }
-
-
             else{
 
                 this.modelObjetos[0]['id'] = this.objetos[index]['id'];
@@ -307,95 +174,37 @@ var app = new Vue({
         },
         desativarObjeto: function(classe, id){
             if( confirm("Tem certeza que deseja deletar? "+id) == true){
-                /*
-                if(classe !== "diario"){
-                    var url = '/api/'+classe+'/'+id+'?ativo=0';
-                    alert(url);
-                    fetch(url, { method: 'PUT'} ).catch((e) => this.error = e);
-                    if(this.error == null){
-                        //this.limparModal();
-                        //this.modalSucesso = true;
-                        alert("Desativado com sucesso");
-                    }
-                    else{
-                        //this.modalErro = true;
-                        alert("Um erro foi encontrado:"+this.error);
-                        //this.modalErro = false;
-                        //this.error = null;
-                    }
-                    this.carregarObjeto(classe);
-                }
-                else{
-                    */
                     var url = '/api/'+classe+'/'+id;
-                    //alert(url);
                     fetch(url, { method: 'DELETE'} ).catch((e) => this.error = e);
                     if(this.error == null){
-                        //this.limparModal();
-                        //this.modalSucesso = true;
                         alert("Excluido com sucesso");
                     }
                     else{
-                        //this.modalErro = true;
                         alert("Um erro foi encontrado:"+this.error);
-                        //this.modalErro = false;
-                        //this.error = null;
                     }
                     this.carregarObjeto(classe);
-                //}
-
             }
             else{
                 alert("Cancelado");
             }
         },
         limparModal: function(){
-            //this.modelObjetos= [{}];
-
-            this.modelObjetos[0]['setup'] = "";
-            this.modelObjetos[0]['simbolo'] = "";
-            this.modelObjetos[0]['emocao'] = "";
-            this.modelObjetos[0]['compraVenda'] = "";
-            this.modelObjetos[0]['quantidade'] = "";
-            this.modelObjetos[0]['pontos'] = "";
-            this.modelObjetos[0]['valorEntrada'] = "";
-            this.modelObjetos[0]['valorSaida'] = "";
-            this.modelObjetos[0]['taxa'] = "";
-            this.modelObjetos[0]['dtEntrada'] = "";
-            this.modelObjetos[0]['dtSaida'] = "";
-            this.modelObjetos[0]['fdEmocional'] = "";
-            this.modelObjetos[0]['fdImediato'] = "";
-            this.modelObjetos[0]['nomeEmocao'] = "";
-            this.modelObjetos[0]['nomeSetup'] = "";
 
             this.modelObjetos[0]['name'] = "";
             this.modelObjetos[0]['company_name'] = "";
             this.modelObjetos[0]['email'] = "";
             this.modelObjetos[0]['cnpj'] = "";
 
-            this.modelObjetos[0]['key'] = "";
-            this.modelObjetos[0]['secret'] = "";
-
             this.modelObjetos[0]['senha'] = "";
             this.modelObjetos[0]['confirmaSenha'] = "";
 
 
         },
-        mostrarConteinerObjeto: function(classe){
-            if(classe !== ''){
-                this.carregarObjeto(classe);
-            }
-        },
         limparGeral: function(){
-            //alert('teste');
-            //limparModal();
             this.objetos = null;
             this.titulo = "";
             this.nomeObjeto = "";
             this.acaoObjeto = "";
-            this.apiObjetos = null;
-            this.credencialApi = null;
-            //alert('teste2');
         },
         verificaDados: function(classe){
             if(classe == 'cliente'){
