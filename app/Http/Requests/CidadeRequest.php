ecdf527f8c1b138966d83368e622c4bd4fc0bd96
rafaelmelo93 <rafa93melo@gmail.com>
2023-05-11 20:10:17 -0300

    Validação cidade - By Rafael

---
+++ app/Http/Requests/CidadeRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CidadeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name_city' => 'required|max: 45',
+            'estado_id' => 'required|integer'
+            /*'modelo' => 'required|max:50',
+            'ano' => 'required|integer|min:1900',
+            'km' => 'required|integer',
+            'placa' => 'required|max:7'*/
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name_city.required' => 'Nome é obrigatório',
+            'name_city.max' => 'Máximo 45 caracteres no nome',
+
+            'estado_id.required' => 'O ID da cidade é obrigatório',
+            'estado_id.integer' => 'O estado deve ser inteiro'
+            
+
+
+            /*'modelo.required' => 'Mdelo é obrigatório',
+            'modelo.max' => 'modelo máximo 50 caracteres',
+
+            'ano.min' => 'Ano deve ser maior que 1900',
+            'ano.integer' => 'Ano deve ser um número inteiro',
+            'ano.required' => 'Ano é obrigatórip',
+
+            'km.integer' => 'Km deve ser inteiro',
+            'km.required' => 'Km é obrigatório',
+
+            'placa.required' => 'Placa é obrigatório',
+            'placa.max' => 'Placa maximo de 7 caracteres'*/
+        ];
+    }
+}
