7dc6398cadff8e5a1251d278bb1f02209ecdfeaa
rafaelmelo93 <rafa93melo@gmail.com>
2023-05-18 22:05:28 -0300

    Ícone na aba do navegador - By Rafael

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" href="./logo.ico" type="image/x-icon">
     <title>
         Salgados Zilla
     </title>
