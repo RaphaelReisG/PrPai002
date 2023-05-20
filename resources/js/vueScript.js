86e26fb123774d83a60cfebac632078ba36f9c7f
Murillo Nonato Silva <murillo.murillo2001@gmail.com>
2023-05-20 17:25:14 -0300

    Mensagens de erro para alteração de Administrador e Vendedor - By Murillo
    Mensagens de erro para alteração de Administrador e Vendedor

--- resources/js/vueScript.js
+++ resources/js/vueScript.js
@@ -1216,13 +1216,28 @@ var app = new Vue({
                     }
                 }
                 else if(this.acaoObjeto == "Alterar"){
-                    if(
-                        this.modelObjetos[0]['name'] == "" ||
-                        this.modelObjetos[0]['email'] == ""
-                    ){
-                        alert("Erro");
-                        return true;
+                    var error = false
+                    if(this.modelObjetos[0]['name'] == ""){
+                        this.alertaCampo[0]['name'] = "O nome é obrigatório";
+                        error = true
                     }
+                    if (this.modelObjetos[0]['name'].length > 45) {
+                        this.alertaCampo[0]['name'] = "O nome deve ter no máximo 45 caracteres";
+                        error = true
+                    }
+                    if(this.modelObjetos[0]['email'] == ""){
+                        this.alertaCampo[0]['email'] = "O e-mail é obrigatório";
+                        error = true
+                    }
+                    if (this.modelObjetos[0]['email'].length > 45) {
+                        this.alertaCampo[0]['email'] = "O e-mail deve ter no máximo 45 caracteres";
+                        error = true
+                    }
+
+                    if(error == true) {
+                        return true
+                    }
+
                     else{
                         return false;
                     }
